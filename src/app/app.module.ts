81f7d93529e4f32c153a71a49bcc8c6ec2ddf791
shaheerwmk <shaheerwmk07@live.com>
2019-06-18 20:22:35 -0400

    Applied the first route from home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component';
+
+const appRoutes: Routes = [
+  { path: 'user', component: UserComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
